d4409b5674c84782b400f9cd23488763829aec30
Christian Brock <Christiansbrock@gmail.com>
2024-02-23 17:26:19 -0600

    Updated to latest WPI version

--- src/main/java/frc/robot/commands/limelight/LineUpToAmp.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands.limelight;
-
-import edu.wpi.first.wpilibj2.command.Command;
-
-public class LineUpToAmp extends Command {
-    public LineUpToAmp() {
-        addRequirements(null);
-    }
-}
